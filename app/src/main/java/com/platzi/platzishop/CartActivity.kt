130c32cf40b12fdf91ecb6df412fcb1637456d8a
Sebastian Tellez <sebastian.devmobile@gmail.com>
2017-09-14 05:39:20 -0500

    more more more

--- app/src/main/java/com/platzi/platzishop/CartActivity.kt
+++ app/src/main/java/com/platzi/platzishop/CartActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_cart.*
 
 class CartActivity : AppCompatActivity() {
@@ -11,6 +13,10 @@ class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+
+        toolbarCart.title = "Carrito"
+        setSupportActionBar(toolbarCart)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
@@ -24,4 +30,17 @@ class CartActivity : AppCompatActivity() {
         rcCart.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         rcCart.adapter = AdapterCart(items)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cart, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/platzi/platzishop/LandingActivity.kt
+++ app/src/main/java/com/platzi/platzishop/LandingActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_landing.*
 
 class LandingActivity : AppCompatActivity() {
@@ -18,11 +19,23 @@ class LandingActivity : AppCompatActivity() {
         rcLanding.layoutManager = GridLayoutManager(this, 2) as RecyclerView.LayoutManager
         rcLanding.setHasFixedSize(true)
 
-        val itemsShop = (0..20).map { Itemlanding("Item $it", "Descripcion $it", 200.00, "") }
+        val itemsShop = (0..20).map {
+            Itemlanding("Item $it", "Descripcion $it", 200.00 + it, "")
+        }.filter {
+            it.price > 201
+        }.sortedByDescending {
+            it.price
+        }
 
         val adapter = AdapterLanding(itemsShop)
 
         rcLanding.adapter = adapter
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_landing, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
